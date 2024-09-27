90f85826f6e995220cd7ad1f49ba585e5c05d7b6
MazzolimLucas <127057438+MazzolimLucas@users.noreply.github.com>
2024-09-26 21:57:01 -0300

    Update Sidebar.js

--- Componentes Reutilizáveis/Sidebar.js
+++ Componentes Reutilizáveis/Sidebar.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import '../Estilos/Sidebar.css'
+import {Link} from 'react-router-dom'
+
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <h2>MediBED</h2>
-      <div className="user-info">
+      <div className="userInfo">
         <img src="https://via.placeholder.com/50" alt="User" />
-        <h4>Ahmed Khalid</h4>
-        <span>Online</span>
+        <h4>Lucas William</h4>          
       </div>
-      <nav>
-        <ul>
-          <li><a href="#doctors">Doctors</a></li>
-        </ul>
+      <nav className='sidebarNavigation'>
+        <div className='sidebarTitle'>
+          <p>Menu</p>
+        </div>
+        <div className='paginaAlunos'>
+          <Link to="/Home/Alunos" className='paginaAlunosLink primeiroLink'>Alunos</Link>
+          <Link to="/Home/Avaliacoes" className='paginaAlunosLink'>Avaliações</Link>
+          <Link to="/Home/Capacidades" className='paginaAlunosLink'>Capacidades</Link>
+          <Link to="/Home/Critérios de Avaliação" className='paginaAlunosLink'>Criterios de Avaliação</Link>
+          <Link to="/Home/Disciplinas" className='paginaAlunosLink'>Disciplinas</Link>
+          <Link to="/Home/Planos de Ensino" className='paginaAlunosLink'>Planos de Ensino</Link>
+          <Link to="/Home/Professores" className='paginaAlunosLink'>Professores</Link>
+          <Link to="/Home/Turmas" className='paginaAlunosLink'>Turmas</Link>
+        </div>
       </nav>
     </div>
   );
